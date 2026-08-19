b479ebddbf2ba63a35b214c4a26134a277a3d76d
Aadit Anand Fadia <aaf9407@nyu.edu>
2026-08-19 14:33:01 -0400

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = defaultdict(list)
+        for u, v in prerequisites:
+            adj[v].append(u)
+        state = [0]*numCourses
+        def hasCycle(node):
+            if state[node] == 1:
+                return True
+            
+            if state[node] == 2:
+                return False
+            
+            state[node] = 1
+
+            for neighbor in adj[node]:
+                if hasCycle(neighbor):
+                    return True
+            
+            state[node] = 2
+            return False
+        
+        for i in range(numCourses):
+            if state[i] == 0:
+                if hasCycle(i):
+                    return False
+        return True
+
+
